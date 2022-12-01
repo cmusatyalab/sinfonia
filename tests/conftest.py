0c67cd05cd24637e0e5577b2b37ea56b6d1f3562
Jan Harkes <jaharkes@cs.cmu.edu>
2022-11-30 23:07:00 -0500

    Remove tier3 component, it was split off as sinfonia-tier3

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@
 from uuid import UUID
 
 import pytest
-import wgconfig.wgexec
 from flask import Flask
 from geolite2 import geolite2
 
@@ -50,30 +49,6 @@ def bad_uuid():
 @pytest.fixture(scope="session")
 def restricted_uuid():
     return UUID(RESTRICTED_UUID)
-
-
-@pytest.fixture
-def mock_generate_keypair(monkeypatch):
-    """wgconfig.wgexec.generate_keypairs mocked so we don't `wg` binary"""
-    keypairs = [
-        (
-            "mHJFze/rYugSqH5y5jYgJmJA+Xn+8GYankWDJx69Ymo=",
-            "LaMgyk/jPiVRX1XFhBbbW7RlZQO976ZOcnpjlRIeSCc=",
-        ),
-        (
-            "AB9y9TPUpZRYXdA/VEMmY1vjXN78xnG3W5u0kh+7H3c=",
-            "P8+7aAk2FsUYkhX4CvJfFWWThus25+A9AeoIRdeEumU=",
-        ),
-        (
-            "wDKetfz9LiQq1hu4E8x0woPmwFp/Oc6Zt69gglQHsV8=",
-            "nyJ86rdfI7nxVk7CBoDV42e6gh6E2EzAbI/dVTGbdjs=",
-        ),
-    ]
-
-    def generate_keypair():
-        return keypairs.pop()
-
-    monkeypatch.setattr(wgconfig.wgexec, "generate_keypair", generate_keypair)
 
 
 @pytest.fixture(scope="session")
